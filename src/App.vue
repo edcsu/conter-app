6c70f6b1b4bf76cea45d9a387556db93da3208d3
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-04-07 17:26:55 +0300

    refactor: set functions in JavaScript tag

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <script setup>
-  import { ref } from "vue";
+import { ref } from "vue";
 
-  const count = ref(0);
+const count = ref(0);
+
+const addToCount = () => {
+  count.value++
+}
+
+const subtractFromCount = () => {
+  count.value--
+}
+
 </script>
 
 <template>
-  <div>
-    <h3>
-      The current count is 
-    </h3>
-    <h1 class="text-center">
-      {{count}}
-    </h1>
-    <button class="btn red-background white-text" @click="count--">-</button>
-    <button class="btn green-background white-text" @click="count++">+</button>
-  </div>
+  <main>
+    <div>
+      <h3>
+        The current count is
+      </h3>
+      <h1 class="text-center">
+        {{ count }}
+      </h1>
+      <button class="btn red-background white-text" @click="subtractFromCount">-</button>
+      <button class="btn green-background white-text" @click="addToCount">+</button>
+    </div>
+  </main>
 </template>
